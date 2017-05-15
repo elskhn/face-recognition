a13a215ba8891b797c139dc069ad7ba2ab0a3b59
Abdullah K <abdullahkhan0503@gmail.com>
2017-05-15 14:51:49 -0600

    Update create_data.py
    Added comments for further clarification

--- create_data.py
+++ create_data.py
@@ -11,7 +11,9 @@ datasets = '/home/USER/Workspaces/Python/openCV/Facial-recognition/faces'
 # change the name below when creating a new dataset for a new person
 sub_dataset = 'Abdullah'
 
+# join the paths to include the sub_dataset folder
 path = os.path.join(datasets, sub_dataset)
+# if sub_dataset folder doesn't already exist, make the folder with the name defined above
 if not os.path.isdir(path):
     os.mkdir(path)
 
@@ -21,22 +23,30 @@ if not os.path.isdir(path):
 face_cascade = cv2.CascadeClassifier(haar_file)
 # use '0' for internal (built-in) webcam or '1' for external ones
 webcam = cv2.VideoCapture(0)
+# returns true or false (if the camera is on or not)
 print("Webcam is open? ", webcam.isOpened())
-time.sleep(1)
+# wait for the camera to turn on (just to be safe, in case the camera needs time to load up)
+time.sleep(2)
 #Takes pictures of detected face and saves them
 count = 1
 print("Taking pictures...")
-# this takes 30 pictures of your face. Change this number if you want the classifier to be more accurate.
-# Having too many images, however, might slow down the program and use more of your CPU (to train the classifier)
+# this takes 30 pictures of your face. Change this number if you want.
+# Having too many images, however, might slow down the program and use more of your CPU
 while count < 31:
+    # im = camera stream
     ret_val, im = webcam.read()
+    # if it recieves something from the webcam...
     if ret_val == True:
         gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        # detect face using the haar cascade file
         faces = face_cascade.detectMultiScale(gray, 1.3, 4)
         for (x,y,w,h) in faces:
+            # draws a rectangle around your face when taking pictures
+            # this is to create a ROI (region of interest) so it only takes pictures of your face
             cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
+            # define 'face' as the inside of the rectangle we made above and make it grayscale
             face = gray[y:y + h, x:x + w]
-            # resize the face images for consistency
+            # resize the face images to the size of the 'face' variable above (i.e: area captured inside of the rectangle)
             face_resize = cv2.resize(face, (width, height))
             # save images with their corresponding number
             cv2.imwrite('%s/%s.png' % (path,count), face_resize)
@@ -44,6 +54,7 @@ while count < 31:
         # display the openCV window
         cv2.imshow('OpenCV', im)
         key = cv2.waitKey(20)
+        # press esc to stop the loop
         if key == 27:
             break
 print("Sub dataset for your face has been created.")
